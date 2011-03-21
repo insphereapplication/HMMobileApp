57a77d8265b972396257b3791b56d7b6c4e04d94
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-20 20:40:07 -0500

    adding settings file

---
+++ app/Settings/settings.rb
@@ -0,0 +1,38 @@
+
+class Settings
+  include Rhom::PropertyBag
+
+  class << self
+    
+    def credentials
+      [login,password]
+    end
+    
+    def clear_credentials
+      login=nil
+      password=nil
+    end
+    
+    def login
+      instance.login
+    end
+    
+    def password
+      instance.password
+    end
+    
+    def login=(login)
+      instance.login=login
+      instance.save
+    end
+    
+    def password=(password)
+      instance.password=password
+      instance.save
+    end
+    
+    def instance
+      @instance ||= Settings.find(:first) || Settings.create({})
+    end
+  end
+end
