ee59f05c45da306f9667f468b64fe3c126c9393b
Kyle Norton <kjn592@Kyle-Nortons-MacBook-Pro.local>
2011-04-29 17:27:51 -0500

    Commented out app_start methods

--- app/application.rb
+++ app/application.rb
@@ -17,35 +17,35 @@ class AppApplication < Rho::RhoApplication
     Rhom::Rhom.database_full_reset
   end
   
-  def on_activate_app
-    # put your application activation code here
-  end 
-
-  def on_deactivate_app
-    # Don't call any UI operations here, they'll be ignored
-    # For example, WebView.refresh
-
-    # to stop sync background thread call 
-    # SyncEngine.stop_sync; SyncEngine.set_pollinterval(0)
-
-    # To stop local web server when application switched to 
-    # background return "stop_local_server"
-    Rho::AsyncHttp.cancel(cancel_callback = "*")
-  end
-  
-  def on_ui_created
-      # put your application UI creation code here
-      # for example, create tab bar:
-      # NativeBar.create(Rho::RhoApplication::TABBAR_TYPE, tabs)
-
-      super.on_ui_created() # To navigate to start_path from rhoconfig.txt
-  end
-  
-  def on_ui_destroyed
-      # put your code here
-      # example:
-      # @forbid_ui_operations = true
-  end
+  # def on_activate_app
+  #   # put your application activation code here
+  # end 
+  # 
+  # def on_deactivate_app
+  #   # Don't call any UI operations here, they'll be ignored
+  #   # For example, WebView.refresh
+  # 
+  #   # to stop sync background thread call 
+  #   # SyncEngine.stop_sync; SyncEngine.set_pollinterval(0)
+  # 
+  #   # To stop local web server when application switched to 
+  #   # background return "stop_local_server"
+  #   Rho::AsyncHttp.cancel(cancel_callback = "*")
+  # end
+  # 
+  # def on_ui_created
+  #     # put your application UI creation code here
+  #     # for example, create tab bar:
+  #     # NativeBar.create(Rho::RhoApplication::TABBAR_TYPE, tabs)
+  # 
+  #     #super.on_ui_created() # To navigate to start_path from rhoconfig.txt
+  # end
+  # 
+  # def on_ui_destroyed
+  #     # put your code here
+  #     # example:
+  #     # @forbid_ui_operations = true
+  # end
   
   
   
