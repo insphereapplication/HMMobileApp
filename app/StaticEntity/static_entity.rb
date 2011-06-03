4af1e4a79e72a02d37137c74c82ede7c619ea414
Greg Norz <greg.norz@parivedasolutions.com>
2011-06-03 16:07:09 -0500

    Adding in StaticEntity method additions.

--- app/StaticEntity/static_entity.rb
+++ app/StaticEntity/static_entity.rb
@@ -3,4 +3,19 @@ class StaticEntity
 
   enable :sync
   set :schema_version, '1.0'
+  
+  def self.get_carrier_names
+    carriers = StaticEntity.find('carriers')
+    carrier_names = carriers.names.split('||')
+  end
+  
+  def self.get_lob_names
+    lob = StaticEntity.find('line_of_business')
+    lob_names = lob.names.split('||')
+  end
+  
+  def self.get_lead_source_names
+    lead_source = StaticEntity.find('lead_source')
+    lead_source_names = lead_source.names.split('||')
+  end
 end
