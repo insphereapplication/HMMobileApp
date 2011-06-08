3e3d21fd59b5e6aae1da9de13abe5fd1e47c1fe0
Carter Stubbs <carterstubbs@rho.local>
2011-06-08 16:45:17 -0500

    Added new 'collapsible' button to top of Contacts list for filtering.

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -23,6 +23,79 @@ function loadContactsAsync(page){
 	);
 }
 
+function loadContactsWithActivePolsAsync(page){
+	$.post('/app/Contact/get_contacts_with_active_page', { page: page },
+		function(contacts) {				
+			if (contacts && $.trim(contacts) != ""){
+				$('ul#contact2-list2').append(contacts);
+				
+				$('.group2-divider2').each(function(){
+				  var ids = $('[id='+this.id+']');
+				  if(ids.length>1 && ids[0]==this)
+				    $(ids[1]).remove();
+				});
+				
+				loadContactsWithActivePolsAsync(page + 1);
+			} 
+		}
+	);
+}
+
+function loadContactsWithPendingPolsAsync(page){
+	$.post('/app/Contact/get_contacts_with_pending_page', { page: page },
+		function(contacts) {				
+			if (contacts && $.trim(contacts) != ""){
+				$('ul#contact3-list3').append(contacts);
+				
+				$('.group-divider3').each(function(){
+				  var ids = $('[id='+this.id+']');
+				  if(ids.length>1 && ids[0]==this)
+				    $(ids[1]).remove();
+				});
+				
+				loadContactsWithPendingPolsAsync(page + 1);
+			} 
+		}
+	);
+}
+
+function loadContactsWithOpenOppsAsync(page){
+	$.post('/app/Contact/get_contacts_with_open_opps_page', { page: page },
+		function(contacts) {				
+			if (contacts && $.trim(contacts) != ""){
+				$('ul#contact4-list4').append(contacts);
+				
+				$('.group-divider4').each(function(){
+				  var ids = $('[id='+this.id+']');
+				  if(ids.length>1 && ids[0]==this)
+				    $(ids[1]).remove();
+				});
+				
+				loadContactsWithOpenOppsAsync(page + 1);
+			} 
+		}
+	);
+}
+
+function loadContactsWithWonOppsAsync(page){
+	$.post('/app/Contact/get_contacts_with_won_opps_page', { page: page },
+		function(contacts) {				
+			if (contacts && $.trim(contacts) != ""){
+				$('ul#contact5-list5').append(contacts);
+				
+				$('.group-divider5').each(function(){
+				  var ids = $('[id='+this.id+']');
+				  if(ids.length>1 && ids[0]==this)
+				    $(ids[1]).remove();
+				});
+				
+				loadContactsWithWonOppsAsync(page + 1);
+			} 
+		}
+	);
+}
+
+
 function validateNewContactInfo(){
 	
 	if ( document.getElementById('contact_firstname').value.length==0 || document.getElementById('contact_firstname').value==null ) {
