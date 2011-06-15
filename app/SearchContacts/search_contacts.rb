5de7ae7e59b0b131018637a5b1051617fbcb348f
Vishal Bardoloi <vishal.bardoloi@parivedasolutions.com>
2011-06-15 18:25:19 -0500

    Adding SearchContacts model

---
+++ app/SearchContacts/search_contacts.rb
@@ -0,0 +1,17 @@
+class SearchContacts
+  include Rhom::FixedSchema
+  include ChangedFlag
+  include SQLHelper
+  
+  set :schema_version, '1.0'
+  enable :sync
+  set :sync_priority, 20  
+      
+    
+  def self.search_my_contacts(params=nil)
+    puts "****************************Searching contacts! #{params['first_name']}."
+    result = SearchContacts.create(params)
+    #SyncEngine.dosync
+  end
+  
+end
