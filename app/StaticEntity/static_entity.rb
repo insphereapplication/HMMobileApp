f26f8957a3322ab0451fc6045cefb0d1eca80b6a
Dave Sims <davsims@gmail.com>
2011-06-15 16:36:46 -0500

    call 'first' for find_by_sql

--- app/StaticEntity/static_entity.rb
+++ app/StaticEntity/static_entity.rb
@@ -7,22 +7,22 @@ class StaticEntity
   set :schema_version, '1.0'
   
   def self.get_carrier_names
-    carriers = StaticEntity.find_by_sql("select * from StaticEntity where type='carriers'")
+    carriers = StaticEntity.find_by_sql("select * from StaticEntity where type='carriers'").first
     carrier_names = carriers.names.split('||').sort
   end
   
   def self.get_lob_names
-    lob = StaticEntity.find_by_sql("select * from StaticEntity where type='line_of_business'")
+    lob = StaticEntity.find_by_sql("select * from StaticEntity where type='line_of_business'").first
     lob_names = lob.names.split('||')
   end
 
   def self.get_rawlead_lob_names
-    lob = StaticEntity.find_by_sql("select * from StaticEntity where type='rawlead_lineofbusiness'")
+    lob = StaticEntity.find_by_sql("select * from StaticEntity where type='rawlead_lineofbusiness'").first
     lob_names = lob.names.split('||')
   end
   
   def self.get_lead_source_names
-    lead_source = StaticEntity.find_by_sql("select * from StaticEntity where type='lead_source'")
+    lead_source = StaticEntity.find_by_sql("select * from StaticEntity where type='lead_source'").first
     lead_source_names = lead_source.names.split('||')
   end
 end
