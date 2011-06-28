3ce0a9fb4a3ab371d7e4d7c7d1ba2c1c18497651
Dwayne Smith <dwayne.smith@healthmarkets.com>
2011-06-28 10:33:59 -0500

    Changes for Resouce Center SSO storing key in DB

--- app/helpers/crypto.rb
+++ app/helpers/crypto.rb
@@ -1,8 +1,9 @@
 require 'crypt/rijndael'
 require 'base64'
 
-module Crypto
-  @@rijndael = Crypt::Rijndael::new( Rho::RhoConfig.crypt_key)
+
+class Crypto
+  @@rijndael = Crypt::Rijndael::new( AppInfo.instance.mobile_crypt_key )
   
   def self.encrypt(value)
     encryptedValue = @@rijndael.encrypt_string( value )
@@ -10,7 +11,7 @@ module Crypto
   end # self.encrypt
   
   def self.encryptBase64(value)
-    encryptedValue = Base64.encode64(@@rijndael.encrypt_string(value))
+    encryptedValue = Base64.encode64(@@rijndael.encrypt_string( value ))
     return encryptedValue
   end # self.encrypt
   
@@ -21,7 +22,7 @@ module Crypto
   end # self.decrypt
   
   def self.decryptBase64(value)
-    decryptedValue = @@rijndael.decrypt_string(Base64.decode64(value))
+    decryptedValue = @@rijndael.decrypt_string(Base64.decode64( value ))
     
     return decryptedValue
   end # self.decrypt
