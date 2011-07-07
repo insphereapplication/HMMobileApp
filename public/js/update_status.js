97343301d4bcba092be3389aec283779a844490b
Kyle Norton <kyle.norton@parivedasolutions.com>
2011-07-07 18:53:39 -0500

    prevent double submit on left message and no contact made

--- public/js/update_status.js
+++ public/js/update_status.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   	$('.UpdateStatus').click(function() {
-
+   	$('a.UpdateStatus').click(function() {
+		
 		var apptids = $('input:checkbox:checked.custom').map(function(){
 			return this.value
 		});
@@ -8,8 +8,14 @@ $(document).ready(function() {
 		notes = '&notes=' +  $('#status_note').val();
 		appointments = '&appointments[]=' + $.makeArray(apptids).join('&appointments[]=');
 		window.location.href = $(this).attr('href') + appointments + notes;
+		$(this).attr("href", "#");
 		return false;
+		
+	});
+	
+	$('li.NoContact').click(function(e) {
+	    $(this).removeAttr('href');
 	});
 	
 
- });
+});
