871f4e516408bacfdd04397f4e0e7568ba8b2d73
Dave Sims <davsims@gmail.com>
2011-07-08 16:57:28 -0500

    Fix for error message on multiple status update clicks

--- public/js/update_status.js
+++ public/js/update_status.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-   	$('a.UpdateStatus').click(function() {
-		
-		var apptids = $('input:checkbox:checked.custom').map(function(){
+  $('a.UpdateStatus').click(function() {
+		$(this).unbind("click");
+	  var apptids = $('input:checkbox:checked.custom').map(function(){
 			return this.value
 		});
 		
 		notes = '&notes=' +  $('#status_note').val();
 		appointments = '&appointments[]=' + $.makeArray(apptids).join('&appointments[]=');
-		window.location.href = $(this).attr('href') + appointments + notes;
-		$(this).attr("href", "#");
+		var redirectUrl = $(this).attr('href') + appointments + notes;
+		window.location.href = redirectUrl;
 		return false;
 		
 	});
