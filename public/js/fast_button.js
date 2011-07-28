9767cd8a6845ba2187d142a96267933b8edc30b0
Dave Sims <davsims@gmail.com>
2011-07-28 16:02:45 -0500

    every body must get fast-buttoned

--- public/js/fast_button.js
+++ public/js/fast_button.js
@@ -1,90 +1,106 @@
 $(document).ready(function() {
-	
-	
-  $('a.fast-button').each(function() {
-	  var element = $(this).get(0);
-	  var href = $(this).attr('href');
-	  new FastButton(element, function(){
-		  window.location.href = href;
-		});
-	});
-	
-	// make all submits fast buttons
-	$('input:submit').each(function() {
-	  var element = $(this).get(0);
-	  var button = $(this);
-	  new FastButton(element, function(){
-		  alert('hey');
-			// button.click();
-		});
-	});
-	
+
+    //   $('a.fast-button').each(function() {
+    //   var element = $(this).get(0);
+    //   var href = $(this).attr('href');
+    //   var button = $(this)
+    //   new FastButton(element, function(){
+    // 	  alert("fast anchor");
+    // 	  // window.location.href = href;
+    // 	  button.click();
+    // 	});
+    // });
+    // make all submits fast buttons
+
+    $('input:submit, a').each(function() {
+        var element = $(this).get(0);
+        var button = $(this);
+        var href = $(this).attr('href');
+        new FastButton(element,
+        function() {
+            if (href && href != '') {
+                window.location.href = href;
+            }
+            else {
+                button.click();
+            }
+        });
+    });
+
 });
 
 function FastButton(element, handler) {
-   this.element = element;
-   this.handler = handler;
-   element.addEventListener('touchstart', this, false);
+    this.element = element;
+    this.handler = handler;
+    element.addEventListener('touchstart', this, false);
 };
 
 FastButton.prototype.handleEvent = function(event) {
-   switch (event.type) {
-      case 'touchstart': this.onTouchStart(event); break;
-      case 'touchmove': this.onTouchMove(event); break;
-      case 'touchend': this.onClick(event); break;
-      case 'click': this.onClick(event); break;
-   }
+    switch (event.type) {
+    case 'touchstart':
+        this.onTouchStart(event);
+        break;
+    case 'touchmove':
+        this.onTouchMove(event);
+        break;
+    case 'touchend':
+        this.onClick(event);
+        break;
+    case 'click':
+        this.onClick(event);
+        break;
+    }
 };
 
 FastButton.prototype.onTouchStart = function(event) {
-   event.stopPropagation();
-   this.element.addEventListener('touchend', this, false);
-   document.body.addEventListener('touchmove', this, false);
-   this.startX = event.touches[0].clientX;
-   this.startY = event.touches[0].clientY;
-   this.element.style.backgroundColor = "rgba(0,0,0,.7)";
+    event.stopPropagation();
+    this.element.addEventListener('touchend', this, false);
+    document.body.addEventListener('touchmove', this, false);
+    this.startX = event.touches[0].clientX;
+    this.startY = event.touches[0].clientY;
+    this.element.style.backgroundColor = "rgba(0,0,0,.7)";
 };
 
 FastButton.prototype.onTouchMove = function(event) {
-   if(Math.abs(event.touches[0].clientX - this.startX) > 10 || Math.abs(event.touches[0].clientY - this.startY) > 10) {
-      this.reset();
-   }
+    if (Math.abs(event.touches[0].clientX - this.startX) > 10 || Math.abs(event.touches[0].clientY - this.startY) > 10) {
+        this.reset();
+    }
 };
 
 FastButton.prototype.onClick = function(event) {
-   event.stopPropagation();
-   this.reset();
-   this.handler(event);
-   if(event.type == 'touchend') {
-      preventGhostClick(this.startX, this.startY);
-   }
-   this.element.style.backgroundColor = "";
+    event.stopPropagation();
+    this.reset();
+    this.handler(event);
+    if (event.type == 'touchend') {
+        preventGhostClick(this.startX, this.startY);
+    }
+    this.element.style.backgroundColor = "";
 };
 
 FastButton.prototype.reset = function() {
-   this.element.removeEventListener('touchend', this, false);
-   document.body.removeEventListener('touchmove', this, false);
-   this.element.style.backgroundColor = "";
+    this.element.removeEventListener('touchend', this, false);
+    document.body.removeEventListener('touchmove', this, false);
+    this.element.style.backgroundColor = "";
 };
 
 function preventGhostClick(x, y) {
-   coordinates.push(x, y);
-   window.setTimeout(gpop, 2500);
+    coordinates.push(x, y);
+    window.setTimeout(gpop, 2500);
 };
 
 function gpop() {
-   coordinates.splice(0, 2);
+    coordinates.splice(0, 2);
 };
 
 function gonClick(event) {
-   for(var i = 0; i < coordinates.length; i += 2) {
-      var x = coordinates[i];
-      var y = coordinates[i + 1];
-      if(Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
-         event.stopPropagation();
-         event.preventDefault();
-      }
-   }
+    for (var i = 0; i < coordinates.length; i += 2) {
+        var x = coordinates[i];
+        var y = coordinates[i + 1];
+        if (Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    }
 };
 
 document.addEventListener('click', gonClick, true);
