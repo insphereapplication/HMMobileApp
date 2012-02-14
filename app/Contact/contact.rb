9033e3ef6c95f6ed369a2fcf956ac09ad540f6ed
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-02-14 14:21:55 -0600

    INS -5602 Fix for QC defects 5506 and 5507

--- app/Contact/contact.rb
+++ app/Contact/contact.rb
@@ -164,9 +164,10 @@ class Contact
     })
   end
   
-  def self.get_search_sql(terms)
-    terms_ary = terms.split(/[\s,]/).reject{|term| term.blank? }
-    terms_ary.blank? ? '' : "and (#{terms_ary.map{|term| "c.firstname like '#{term}%' OR c.lastname like '#{term}%' OR c.emailaddress1 like '#{term}%' OR c.telephone1 like '%#{term}%' OR c.telephone2 like '%#{term}%' OR c.telephone3 like '%#{term}%' OR c.mobilephone like '%#{term}%'" }.join(' OR ')})"
+  def self.get_search_sql(term)
+    #terms_ary = terms.split(/[\s,]/).reject{|term| term.blank? }
+    #terms_ary = terms
+    term.blank? ? '' : "and (c.firstname like '#{term}%' OR c.lastname like '#{term}%' OR c.emailaddress1 like '#{term}%' OR c.telephone1 like '%#{term}%' OR c.telephone2 like '%#{term}%' OR c.telephone3 like '%#{term}%' OR c.mobilephone like '%#{term}%')"
   end
   
   def full_name
