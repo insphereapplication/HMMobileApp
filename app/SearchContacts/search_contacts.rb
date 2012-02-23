ca97ff49da3085300838ce8790ef32c08cce93fd
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-02-23 15:58:32 -0600

    INS-5844 - Go directly to CRM for contact search

--- app/SearchContacts/search_contacts.rb
+++ app/SearchContacts/search_contacts.rb
@@ -3,9 +3,9 @@ class SearchContacts
   include ChangedFlag
   include SQLHelper
   
-  enable :sync
+  #enable :sync
   set :schema_version, '1.0'
-  set :sync_priority, 20  
+  #set :sync_priority, 20  
     
   #returns the hash of contact details that the input id points to
   def self.find_by_id(contactid)
@@ -25,6 +25,42 @@ class SearchContacts
     Rho::JSON.parse(terms)
   end
   
+  def self.search_crm(search_params)  
+    crm_url = Rho::RhoConfig.crm_url
+    user = Settings.login
+    pw = Settings.password
+    attributes = ''
+    add_comma = false;
+    if search_params
+      attributes = "&attributes={"
+      search_params.each { |key, value| 
+         puts "\"#{key}\":\"#{value}\""
+         attributes = attributes + "," if add_comma
+         attributes = attributes + "\"#{key}\":\"#{value}\""
+         add_comma = true
+         }
+       attributes = attributes + "}"   
+      puts "attributes:  #{attributes}" 
+    end
+    result = Rho::AsyncHttp.post(
+      :url => "#{crm_url}/contact/search",
+      :body => "username=#{user}&password=#{pw}&#{attributes}"
+    )
+  
+
+    # removing the ownerid as is used to translate to user in rhosync.   This above is directly calling CRM mobile proxy instead of going through rhosync
+    res = result["body"].map do |value|
+       value.reject!{|k,v|  ['ownerid'].include?(k) }
+       value
+     end
+     
+    res = res.reduce({}){|sum, value| sum[value["contactid"]] = value if value; sum }
+  
+    SearchContacts.create({:object => Time.now.to_i, 	:results => res.to_json, :terms => search_params.to_json})
+  
+  end
+  
+  
   def search_results
     Rho::JSON.parse(results).sort do |a,b|
       comp = (a[1]['lastname'].downcase <=> b[1]['lastname'].downcase)
@@ -38,5 +74,5 @@ class SearchContacts
   
   def find_contacts_on_device
     Contact.find_contacts_on_device(contact_ids)
-   end
+  end
 end
--- app/SearchContacts/search_contacts_controller.rb
+++ app/SearchContacts/search_contacts_controller.rb
@@ -8,28 +8,35 @@ class SearchContactsController < Rho::RhoController
     ### When a new Contact Search is initiated, it deletes the old search data and replaces it with the results of the new search
 
     #TODO: remove last search activity
-    SearchContacts.clear_all_search_results
+    begin
+      SearchContacts.clear_all_search_results
     
-    unless System.has_network
-      WebView.navigate(url_for(:action => :search, :controller => :SearchContacts, :query => {:show_results => 'true', :msg => 'No internet connection. Please check your connection and try again.'}), Constants::TAB_INDEX['Contacts'])
-    else
+      unless System.has_network
+        WebView.navigate(url_for(:action => :search, :controller => :SearchContacts, :query => {:show_results => 'true', :msg => 'No internet connection. Please check your connection and try again.'}), Constants::TAB_INDEX['Contacts'])
+      else
     
-      # perform new search    
-      @last_search_terms = { 
-        :first_name => @params['first_name'],
-        :last_name => @params['last_name'],
-        :full_name => @params['full_name'],
-        :email => @params['email'],
-        :phone => @params['phone']
-      }
-      SearchContacts.search({
-        :from => 'search',
-        :search_params => @last_search_terms,
-        :callback => url_for(:action => :search_callback),
-        :callback_param => ""
-      })
-            
-    end
+        # perform new search    
+        @last_search_terms = { 
+          :first_name => @params['first_name'],
+          :last_name => @params['last_name'],
+          :full_name => @params['full_name'],
+          :email => @params['email'],
+          :phone => @params['phone']
+        }
+        # SearchContacts.search({
+        #    :from => 'search',
+        #    :search_params => @last_search_terms,
+        #    :callback => url_for(:action => :search_callback),
+        #    :callback_param => ""
+        #  })
+      
+        SearchContacts.search_crm(@last_search_terms)        
+        WebView.navigate(url_for(:action => :search, :controller => :SearchContacts, :query => {:show_results => 'true'}), Constants::TAB_INDEX['Contacts'])
+      end
+     rescue Exception => e
+            puts "There was an error attempting to search contacts rolling back: #{e.inspect}"
+            WebView.navigate(url_for(:action => :search, :controller => :SearchContacts, :query => {:show_results => 'true', :msg => 'Unable to retrieve Contacts from Activity Center,  please try narrowing your search'}), Constants::TAB_INDEX['Contacts'])
+     end
   end
   
   def search_callback
