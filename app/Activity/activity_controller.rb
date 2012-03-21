9c1914fe07926ddf17fe31f50d230bb8fdc6897a
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-03-21 14:53:38 -0500

    Changing redirect first like other methods

--- app/Activity/activity_controller.rb
+++ app/Activity/activity_controller.rb
@@ -141,8 +141,9 @@ class ActivityController < Rho::RhoController
 
   def opportunity_details
     Rho::NativeTabbar.switch_tab(0)
-    WebView.navigate(url_for(:controller => :Opportunity, :action => :show, :id => @params['id']), 0)
     redirect :action => :index
+    WebView.navigate(url_for(:controller => :Opportunity, :action => :show, :id => @params['id']), 0)
+ 
   end
 
   # GET /Contact/activity_summary
