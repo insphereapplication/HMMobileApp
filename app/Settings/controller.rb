c3fae12a3481cf806c526ad2ff556bfdd7e4a7d2
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-04-26 12:01:24 -0500

    INS-6147- LeadForward Active Orders

--- app/Settings/controller.rb
+++ app/Settings/controller.rb
@@ -844,6 +844,34 @@ class SettingsController < Rho::RhoController
       end
       
   end
+  
+  def lead_forward_active_orders
+        Settings.record_activity
+        if Settings.pin_confirmed == true
+          resource_url = Rho::RhoConfig.resource_center_url
+          lead_forward_target = Rho::RhoConfig.lead_forward_active_orders_target
+          lead_forward_target_encode = Rho::RhoSupport.url_encode(lead_forward_target)
+          ctime = Time.new.utc
+          ctime_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{ctime}Delimit"))
+          user_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{Settings.login}Delimit"))
+          pwd_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{Settings.password}Delimit"))
+            
+          resource_params_enc = "UserName=#{user_enc}&pwd=#{pwd_enc}&valid=#{ctime_enc}&ReturnURL=#{lead_forward_target_encode}"
+        
+          puts "Resource URL parameters are: ****#{resource_params_enc}****"
+          puts "Current UTC is:  #{ctime}"
+
+          rc_url ="#{resource_url}?#{resource_params_enc}"
+       
+          redirect :action => :index, :back => 'callback:', :layout => 'layout_jquerymobile'
+        
+          System.open_url("#{resource_url}?#{resource_params_enc}")
+          
+      else
+          redirect :action => :index, :back => 'callback:', :layout => 'layout_jquerymobile'
+      end
+      
+  end
 
   def check_for_upgrade
     latest_version = AppInfo.instance.latest_version
