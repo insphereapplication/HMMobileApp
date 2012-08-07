0faa715217bdb03560cd85e6f4258e4084e82aac
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-08-07 14:24:43 -0500

    Setting the default url for opportunity

--- app/Opportunity/opportunity_controller.rb
+++ app/Opportunity/opportunity_controller.rb
@@ -17,7 +17,7 @@ class OpportunityController < Rho::RhoController
     tabbar = [
       { 
         :label => "Opportunities", 
-        :action => '/app/Opportunity', 
+        :action => url_for(:controller => :Opportunity, :action => :index), 
         :icon => "/public/images/dollar.png", 
         :web_bkg_color => 0x7F7F7F
       }, 
