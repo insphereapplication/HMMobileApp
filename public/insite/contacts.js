6e4c7b214b83adb0d9d51073ad4eaa537e1afec7
Alex Mitrofanov <alex.mitrofanov@inspherehq.com>
2012-11-02 15:34:35 -0500

    JQueryMobile-1.2.0 migration

--- public/insite/contacts.js
+++ public/insite/contacts.js
@@ -1,3 +1,22 @@
-function autodividersSelector(li) {
-    return li.text().slice(0, 1).toUpperCase();
+function clearFilter() {
+    $("#contact_filter").val("all");
+    $("#contact_filter").selectmenu("refresh");
+    $("#search_input").val("");
+}
+
+function getFilterData() {
+    return { "filter": $("#contact_filter").val(), "search_terms": $("#search_input").val() };
+}
+
+function getFilterText() {
+    var select = $("#contact_filter")[0];
+    var txt = select.options[select.selectedIndex].text;
+    var index = txt.indexOf(" ");
+    if (index > 0)
+        txt = txt.substr(0, index);
+    var result = txt;
+    txt = $("#search_input").val();
+    if (txt.length > 0)
+        result += ", " + txt;
+    return result;
 }
--- public/insite/list.js
+++ public/insite/list.js
@@ -1,11 +1,12 @@
-function ScrollView(page, url, pageNum, pageSize, lookAheadPages) {
+function ScrollView(page, url, pageSize) {
     var $page = $(page),
         $filter = $page.find(".scrollable-list-filter"),
-        $filterTxt = $filter.find(".scrollable-list-filter-txt"),
+        $filterTxt = $filter.find(".scrollable-list-filter-txt")
+                            .find("span.ui-btn-text"),
         $scroll = $page.find(".iscroll-wrapper"),
-        $list = $scroll.find("ul.ui-listview"),
         scroll = $scroll.jqmData("iscrollview"),
-        topPage, lastPageNum, pageCount, lastPageIndex, pages;
+        $list = $scroll.find("ul.scrollable-listview"),
+        filterTxt, requestData, currentpage, pages;
 
     function adjustScrollView() {
         scroll.resizeWrapper();
@@ -19,18 +20,22 @@ function ScrollView(page, url, pageNum, pageSize, lookAheadPages) {
 
     $filter.find(".scrollable-list-filter-find").click(function() {
         $filter.trigger("collapse");
-        
+        reset();
     });
 
     $filter.find(".scrollable-list-filter-clear").click(function() {
         $filter.trigger("collapse");
-        
+        clearFilter();
+        reset();
     });
 
-    function getContent(pageNum, pageSize, fnc) {
+    function getContent(pageNum, pageSize, reset, fnc) {
+        requestData.page = pageNum;
+        requestData.pageSize = pageSize;
+        requestData.reset = reset;
         $.ajax({
             url: url,
-            data: { page: pageNum, pageSize: pageSize },
+            data: requestData,
             success: function(data) {
                 var rows = data.split("<li");
                 fnc((rows.length > 1) ? rows.slice(1, pageSize) : []);
@@ -39,102 +44,49 @@ function ScrollView(page, url, pageNum, pageSize, lookAheadPages) {
     }
 
     $scroll.bind({
-        iscroll_onpulldown: function() {
-            if (topPage + lookAheadPages > 0) {
-                updateListPages();
-                for (var i = pageCount - 2; i >= 0; i--)
-                    pages[i + 1] = pages[i];
-                pages[0] = [];
-                topPage--;
-                getContent(topPage, pageSize, function(data) {
-                    pages[0] = data;
-                });
-                showPages(false);
-            }
-            else
-                scroll.refresh();
-        },
         iscroll_onpullup: function() {
-            if (pages[lookAheadPages + 2].length > 0) {
-                updateListPages();
-                for (var i = 1; i < pageCount; i++)
-                    pages[i - 1] = pages[i];
-                pages[lastPageIndex] = [];
-                topPage++;
-                getContent(topPage + lastPageIndex, pageSize, function(data) {
-                    pages[lastPageIndex] = data;
+            if (pages[0].length > 0) {
+                var item = $list.find("li").eq($list.length - 5);
+                $list.append("<li" + pages[0].join("<li"));
+                scroll.refresh(null, null, function() {
+                    if (item.length > 0)
+                        scroll.scrollToElement(item[0], 0);
                 });
-                showPages(true);
+                pages[0] = pages[1];
+                getContent(currentpage++, pageSize, false, function(data) {
+                    pages[1] = data;
+                });
             }
             else
                 scroll.refresh();
         }
     });
-
-    $list.listview({
-        autodividersSelector: autodividersSelector
-    });
-
-    function updateListPages() {
-        var content = $list.html().split("<li");
-        var length = content.length;
-        if (length > 1) {
-            content = content.splice(1, length);
-            pages[lookAheadPages] = content.splice(0, pageSize);
-            if (content.length > 0)
-                pages[lookAheadPages + 1] = content;
-        }
-    }
-
-    function showPages(isUp) {
-        var content = "";
-        var halfPage = pages[lookAheadPages];
-        var middleIndex = halfPage.length;
-        if (middleIndex > 0) {
-            content = "<li" + halfPage.join("<li");
-            halfPage = pages[lookAheadPages + 1];
-            if (halfPage.length > 0)
-                content += "<li" + halfPage.join("<li");
-        }
-        $list.empty().append(content).listview("refresh");
-        scroll.refresh(null, null, function() {
-            if (typeof(isUp) !== "undefined" && middleIndex > 5) {
-                var index = isUp ? middleIndex - 5 : middleIndex - 1;
-                var item = $list.find("li").eq(index);
-                if (item.length > 0)
-                    scroll.scrollToElement(item[0], 0);
-            }
-        });
-    }
 
     function reset() {
-        // initialize global variables
-        topPage = pageNum - lookAheadPages;
-        lastPageNum = lookAheadPages + 2;
-        pageCount = lastPageNum + lookAheadPages;
-        lastPageIndex = pageCount - 1;
-        pages = [];
-        // initial data load
-        var rowCount = pageSize * lastPageNum;
-        if (topPage >= 0)
-            rowCount += pageSize * (topPage + 1);
-        getContent((topPage >= 0) ? topPage : 0, rowCount, function(data) {
-            var currPage = topPage;
-            while (currPage < 0) {
-                pages.push([]);
-                currPage++;
+        requestData = getFilterData();
+        var txt = getFilterText();
+        if (txt.length > 0)
+            txt = " " + txt;
+        $filterTxt.text(filterTxt + txt);
+        currentpage = 3;
+        pages = [[], []];
+        $list.empty();
+        getContent(0, pageSize * 3, true, function(data) {
+            if (data.length > 0) {
+                $list.append("<li" + data.splice(0, pageSize).join("<li"));
+                scroll.refresh();
             }
-            while (data.length > 0) {
-                pages.push(data.splice(0, pageSize));
-                currPage++;
-            }
-            while (currPage < lastPageNum) {
-                pages.push([]);
-                currPage++;
-            }
-            showPages();
+            if (data.length > 0)
+                pages[0] = data.splice(0, pageSize);
+            if (data.length > 0)
+                pages[1] = data;
         });
     }
 
+    filterTxt = $filterTxt.html();
+    var index = filterTxt.indexOf("<");
+    if (index > 0)
+        filterTxt = filterTxt.substr(0, index);
+    filterTxt = $.trim(filterTxt);
     reset();
 }
