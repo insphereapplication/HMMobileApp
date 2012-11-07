8e663d76efc3c748c81698469abb8216047482b0
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-11-07 16:14:15 -0600

    Adding Quoting tool link

--- app/Settings/controller.rb
+++ app/Settings/controller.rb
@@ -900,16 +900,6 @@ class SettingsController < Rho::RhoController
         if Settings.pin_confirmed == true
           insphere_url = Rho::RhoConfig.insphere_url
           medicare_soa_target = Rho::RhoConfig.medicare_soa_target
-          # medicare_soa_target_encode = Rho::RhoSupport.url_encode(medicare_soa_target)
-          #       ctime = Time.new.utc
-          #       ctime_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{ctime}Delimit"))
-          #       user_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{Settings.login}Delimit"))
-          #       pwd_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{Settings.password}Delimit"))
-          #         
-          #       resource_params_enc = "UserName=#{user_enc}&pwd=#{pwd_enc}&valid=#{ctime_enc}&ReturnURL=#{medicare_soa_target_encode}"
-          #     
-          #       puts "Resource URL parameters are: ****#{resource_params_enc}****"
-          #       puts "Current UTC is:  #{ctime}"
 
           rc_url ="#{insphere_url}?#{medicare_soa_target}"
        
@@ -922,6 +912,35 @@ class SettingsController < Rho::RhoController
       end
       
   end
+  
+  def quoting_tool
+        Settings.record_activity
+        if Settings.pin_confirmed == true
+          insphere_url = Rho::RhoConfig.insphere_url
+          quoting_tool_target = Rho::RhoConfig.quoting_tool_target
+          ctime = Time.new.utc
+          ctime_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{ctime}Delimit"))
+          user_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{Settings.login}Delimit"))
+          pwd_enc = Rho::RhoSupport.url_encode(Crypto.encryptBase64("Delimit#{Settings.password}Delimit"))
+                  
+          quoting_tool_params_enc = "UserName=#{user_enc}&pwd=#{pwd_enc}&valid=#{ctime_enc}"
+        
+          # puts "Resource URL parameters are: ****#{resource_params_enc}****"
+          # puts "Current UTC is:  #{ctime}"
+
+          
+          #rc_url ="#{insphere_url}?#{quoting_tool_params_enc}"
+       
+          redirect :action => :index, :back => 'callback:', :layout => 'layout_jquerymobile'
+        
+          System.open_url("#{insphere_url}#{quoting_tool_target}?#{quoting_tool_params_enc}")
+          
+      else
+          redirect :action => :index, :back => 'callback:', :layout => 'layout_jquerymobile'
+      end
+      
+  end
+  
 
   def check_for_upgrade
     latest_version = AppInfo.instance.latest_version
